3b1f3376ad25ee3a623c5fcb1b54aba9d1464ad1
neil.trimboy <neil.trimboy@gmail.com>
2012-05-23 11:44:37 +0000

    Remove hard coded # of controllers

--- parseheating.py
+++ parseheating.py
@@ -6,9 +6,12 @@ import shutil
 
 from stats_defn import StatList
 
+number_of_controllers = len(StatList)
+print "No of controllers is %d" % (number_of_controllers)
+
 status = [0,0,0,0,0,0,0,0,0,0,0,0,0] #one bigger than size required
-start = range(12+1)
-stop = range(12+1)
+start = range(number_of_controllers+1)
+stop = range(number_of_controllers+1)
 
 # Make a temporary copy of the csv file
 shutil.copy2('heatmiser_status.csv','heatmiser_status.csv.tmp')
@@ -25,7 +28,7 @@ fin = csv.reader(open('heatmiser_status.csv.tmp', 'r'))
 # Read the first line to get the initial status
 first = fin.next()
 print first
-for i in range(0,12):
+for i in range(0,number_of_controllers):
 	state=first[6+ (i*6)]
 	#print state
 	if (state == '1'):
@@ -48,7 +51,7 @@ for row in fin:
 	#print row
 	something_is_on = 0
 	print "."
-	for i in range(0,12):
+	for i in range(0,number_of_controllers):
 		state=row[6+(i*6)]
 		st = int(state)
 		#print st
@@ -126,7 +129,7 @@ for row in fin:
 # This works with both Excel and Timeline
 localtime = time.asctime( time.localtime(time.time()))
 localtime = time.strftime("%d %b %Y %H:%M:%S +0000", time.localtime(time.time()))
-for i in range(0,12):
+for i in range(0,number_of_controllers):
     if (status[i+1] == 1):
         # This one is currently on
         s = '<event start ="' + start[i+1] + '"\n'
