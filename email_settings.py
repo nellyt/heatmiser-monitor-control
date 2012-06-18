4fc4d81abca30b92b93e0427baa067c95cc3bd1d
neil.trimboy <neil.trimboy@gmail.com>
2012-06-18 00:44:57 +0000

    Add icon Add comments

--- email_settings.py
+++ email_settings.py
@@ -1,7 +1,10 @@
 #
 # Neil Trimboy 2011 
 #
+# These email settings are used for sending alert messages
+# Currently only supports gmail
 
-email_from_addr = "emailaddress@domain"
-gmail_pwd = "password"
-email_to_addr = 'emailaddress2@domain'
+
+email_from_addr = "emailaddress@domain" # Gmail account
+gmail_pwd = "password" # Gmail password
+email_to_addr = 'emailaddress2@domain' # Who the mail is being sent TO
